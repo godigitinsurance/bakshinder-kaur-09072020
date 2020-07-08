a58455738a8994114ee9655ebed8087aa6efc653
godigitinsurance <59437973+godigitinsurance@users.noreply.github.com>
2020-07-08 18:24:31 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,13 @@
+$("#customerName").text("Bakshinder Kaur");
+$("#policyNumber").text("HUK6208585");
+$("#startDate").text("09-Jul-2020");
+$("#endDate").text("08-Jul-2021");
+
+
+$("#type").text("Two Wheeler");
+$("#registrationNumber").text("PB09W5846");
+$("#make").text("HONDA");
+$("#model").text("ACTIVA");
+$("#vehicalSubType").text("DLX");
+$("#engineNo").text("JF50ET1440858");
+$("#chassis").text("ME4JF502KET443350");
